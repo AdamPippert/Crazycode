a61c575dadae6e3c8e0bf3000d3c4a3ac7b3538b
GitHub Action <action@github.com>
2026-01-31 01:22:52 +0000

    chore: generate

--- packages/crazycode/test/cli/github-remote.test.ts
+++ packages/crazycode/test/cli/github-remote.test.ts
@@ -2,15 +2,24 @@ import { test, expect } from "bun:test"
 import { parseGitHubRemote } from "../../src/cli/cmd/github"
 
 test("parses https URL with .git suffix", () => {
-  expect(parseGitHubRemote("https://github.com/AdamPippert/CrazyCode.git")).toEqual({ owner: "AdamPippert", repo: "CrazyCode" })
+  expect(parseGitHubRemote("https://github.com/AdamPippert/CrazyCode.git")).toEqual({
+    owner: "AdamPippert",
+    repo: "CrazyCode",
+  })
 })
 
 test("parses https URL without .git suffix", () => {
-  expect(parseGitHubRemote("https://github.com/AdamPippert/CrazyCode")).toEqual({ owner: "AdamPippert", repo: "CrazyCode" })
+  expect(parseGitHubRemote("https://github.com/AdamPippert/CrazyCode")).toEqual({
+    owner: "AdamPippert",
+    repo: "CrazyCode",
+  })
 })
 
 test("parses git@ URL with .git suffix", () => {
-  expect(parseGitHubRemote("git@github.com:AdamPippert/CrazyCode.git")).toEqual({ owner: "AdamPippert", repo: "CrazyCode" })
+  expect(parseGitHubRemote("git@github.com:AdamPippert/CrazyCode.git")).toEqual({
+    owner: "AdamPippert",
+    repo: "CrazyCode",
+  })
 })
 
 test("parses git@ URL without .git suffix", () => {
@@ -18,11 +27,17 @@ test("parses git@ URL without .git suffix", () => {
 })
 
 test("parses ssh:// URL with .git suffix", () => {
-  expect(parseGitHubRemote("ssh://git@github.com/AdamPippert/CrazyCode.git")).toEqual({ owner: "AdamPippert", repo: "CrazyCode" })
+  expect(parseGitHubRemote("ssh://git@github.com/AdamPippert/CrazyCode.git")).toEqual({
+    owner: "AdamPippert",
+    repo: "CrazyCode",
+  })
 })
 
 test("parses ssh:// URL without .git suffix", () => {
-  expect(parseGitHubRemote("ssh://git@github.com/AdamPippert/CrazyCode")).toEqual({ owner: "AdamPippert", repo: "CrazyCode" })
+  expect(parseGitHubRemote("ssh://git@github.com/AdamPippert/CrazyCode")).toEqual({
+    owner: "AdamPippert",
+    repo: "CrazyCode",
+  })
 })
 
 test("parses http URL", () => {
